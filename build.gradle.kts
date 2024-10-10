29ebe0be49030079cbca147138cbd596baaf8cd3
Patrick Schwager <patrick.schwager@telekom.de>
2024-10-10 15:10:53 +0200

    adapt gradle build to publish release

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,119 +3,190 @@
  *
  * SPDX-License-Identifier: Apache-2.0
  */
+import com.vanniktech.maven.publish.SonatypeHost
 import org.springframework.boot.gradle.tasks.bundling.BootBuildImage
+import java.lang.System.getenv
+import java.net.URI
 
 plugins {
-	java
-	id("org.springframework.boot") version "3.3.3"
-	id("io.spring.dependency-management") version "1.1.6"
-	id("com.citi.helm") version "2.2.0"
-	id("com.citi.helm-publish") version "2.2.0"
-	id("org.cadixdev.licenser") version "0.6.1"
+    java
+    id("org.springframework.boot") version "3.3.3"
+    id("org.jlleitschuh.gradle.ktlint") version "12.1.1"
+    id("io.spring.dependency-management") version "1.1.6"
+    id("org.cadixdev.licenser") version "0.6.1"
+
+    id("com.citi.helm") version "2.2.0"
+    id("com.citi.helm-publish") version "2.2.0"
+    id("net.researchgate.release") version "3.0.2"
+    id("com.vanniktech.maven.publish") version "0.29.0"
 }
 
 group = "ai.ancf.lmos"
-version = "0.0.4-SNAPSHOT"
 
 java {
-	toolchain {
-		languageVersion = JavaLanguageVersion.of(21)
-	}
-}
-helm {
-	charts {
-		create("main") {
-			chartName.set("${project.name}-chart")
-			chartVersion.set("${project.version}")
-			sourceDir.set(file("src/main/helm"))
-		}
-	}
+    toolchain {
+        languageVersion = JavaLanguageVersion.of(21)
+    }
 }
 
 license {
-	include("**/*.java") // Apply license header ONLY to Java files
-	include("**/*.yaml") // Apply license header ONLY to Java files
-	// OR
-	exclude("**/*.properties") // Apply license header NOT to pro
+    include("**/*.java")
+    include("**/*.kt")
+    include("**/*.yaml")
+    exclude("**/*.properties")
+}
+
+helm {
+    charts {
+        create("main") {
+            chartName.set("${project.name}-chart")
+            chartVersion.set("${project.version}")
+            sourceDir.set(file("src/main/helm"))
+        }
+    }
+}
+
+tasks.register("replaceChartVersion") {
+    doLast {
+        val chartFile = file("src/main/helm/Chart.yaml")
+        val content = chartFile.readText()
+        val updatedContent = content.replace("\${chartVersion}", "${project.version}")
+        chartFile.writeText(updatedContent)
+    }
 }
 
 tasks.register("helmPush") {
-	description = "Push Helm chart to OCI registry"
-	group = "helm"
-	dependsOn(tasks.named("helmPackageMainChart"))
+    description = "Push Helm chart to OCI registry"
+    group = "helm"
+    dependsOn(tasks.named("helmPackageMainChart"))
 
-	doLast {
-		val registryUrl = getProperty("REGISTRY_URL")
-		val registryUsername = getProperty("REGISTRY_USERNAME")
-		val registryPassword = getProperty("REGISTRY_PASSWORD")
-		val registryNamespace = getProperty("REGISTRY_NAMESPACE")
+    doLast {
+        val registryUrl = getProperty("REGISTRY_URL")
+        val registryUsername = getProperty("REGISTRY_USERNAME")
+        val registryPassword = getProperty("REGISTRY_PASSWORD")
+        val registryNamespace = getProperty("REGISTRY_NAMESPACE")
 
-		helm.execHelm("registry", "login") {
-			option("-u", registryUsername)
-			option("-p", registryPassword)
-			args(registryUrl)
-		}
+        helm.execHelm("registry", "login") {
+            option("-u", registryUsername)
+            option("-p", registryPassword)
+            args(registryUrl)
+        }
 
-		helm.execHelm("push") {
-			args(tasks.named("helmPackageMainChart").get().outputs.files.singleFile.toString())
-			args("oci://${registryUrl}/${registryNamespace}")
-		}
+        helm.execHelm("push") {
+            args(tasks.named("helmPackageMainChart").get().outputs.files.singleFile.toString())
+            args("oci://$registryUrl/$registryNamespace")
+        }
 
-		helm.execHelm("registry", "logout") {
-			args(registryUrl)
-		}
-	}
+        helm.execHelm("registry", "logout") {
+            args(registryUrl)
+        }
+    }
 }
 
 fun getProperty(propertyName: String) = System.getenv(propertyName) ?: project.findProperty(propertyName) as String
 
 tasks.named<BootBuildImage>("bootBuildImage") {
-	val registryUrl = getProperty("REGISTRY_URL")
-	val registryUsername = getProperty("REGISTRY_USERNAME")
-	val registryPassword = getProperty("REGISTRY_PASSWORD")
-	val registryNamespace = getProperty("REGISTRY_NAMESPACE")
+    val registryUrl = getProperty("REGISTRY_URL")
+    val registryUsername = getProperty("REGISTRY_USERNAME")
+    val registryPassword = getProperty("REGISTRY_PASSWORD")
+    val registryNamespace = getProperty("REGISTRY_NAMESPACE")
 
-	imageName.set("${registryUrl}/${registryNamespace}/${project.name}:${project.version}")
-	publish = true
-	docker {
-		publishRegistry {
-			url.set(registryUrl)
-			username.set(registryUsername)
-			password.set(registryPassword)
-		}
-	}
+    imageName.set("$registryUrl/$registryNamespace/${project.name}:${project.version}")
+    publish = true
+    docker {
+        publishRegistry {
+            url.set(registryUrl)
+            username.set(registryUsername)
+            password.set(registryPassword)
+        }
+    }
+}
+
+mavenPublishing {
+    publishToMavenCentral(SonatypeHost.CENTRAL_PORTAL)
+    signAllPublications()
+
+    pom {
+        name = "LMOS Operator"
+        description =
+            """The LMOS Operator is a Kubernetes operator designed to dynamically resolve Channel requirements based on 
+                the capabilities of installed Agents within a Kubernetes cluster (environment).
+            """.trimMargin()
+        url = "https://github.com/lmos-ai/lmos-operator"
+        licenses {
+            license {
+                name = "Apache-2.0"
+                distribution = "repo"
+                url = "https://github.com/lmos-ai/lmos-operator/blob/main/LICENSES/Apache-2.0.txt"
+            }
+        }
+        developers {
+            developer {
+                id = "telekom"
+                name = "Telekom Open Source"
+                email = "opensource@telekom.de"
+            }
+        }
+        scm {
+            url = "https://github.com/lmos-ai/lmos-operator.git"
+        }
+    }
+
+    release {
+        buildTasks = listOf("releaseBuild")
+        ignoredSnapshotDependencies =
+            listOf()
+        newVersionCommitMessage = "New Snapshot-Version:"
+        preTagCommitMessage = "Release:"
+    }
+
+    tasks.register("releaseBuild") {
+        dependsOn(subprojects.mapNotNull { it.tasks.findByName("build") })
+    }
+
+    repositories {
+        maven {
+            name = "GitHubPackages"
+            url = URI("https://maven.pkg.github.com/lmos-ai/lmos-operator")
+            credentials {
+                username = findProperty("GITHUB_USER")?.toString() ?: getenv("GITHUB_USER")
+                password = findProperty("GITHUB_TOKEN")?.toString() ?: getenv("GITHUB_TOKEN")
+            }
+        }
+    }
 }
 
 repositories {
-	mavenCentral()
+    mavenCentral()
+    mavenLocal()
 }
 
 dependencies {
-	implementation("org.springframework.boot:spring-boot-starter")
-	implementation("org.springframework.boot:spring-boot-starter-web")
-	implementation("org.springframework.boot:spring-boot-starter-webflux")
-	implementation("org.springframework.boot:spring-boot-starter-actuator")
+    implementation("org.springframework.boot:spring-boot-starter")
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-starter-webflux")
+    implementation("org.springframework.boot:spring-boot-starter-actuator")
 
-	implementation("io.javaoperatorsdk:operator-framework-spring-boot-starter:5.5.0")
-	implementation("io.fabric8:generator-annotations:6.13.3")
+    implementation("io.javaoperatorsdk:operator-framework-spring-boot-starter:5.5.0")
+    implementation("io.fabric8:generator-annotations:6.13.3")
 
-	implementation("org.apache.felix:org.apache.felix.resolver:2.0.4")
+    implementation("org.apache.felix:org.apache.felix.resolver:2.0.4")
 
-	implementation("org.semver4j:semver4j:5.3.0")
+    implementation("org.semver4j:semver4j:5.3.0")
 
-	testImplementation("io.javaoperatorsdk:operator-framework-spring-boot-starter-test:5.5.0") {
-		exclude(group = "org.apache.logging.log4j", module = "log4j-slf4j2-impl")
-	}
+    testImplementation("io.javaoperatorsdk:operator-framework-spring-boot-starter-test:5.5.0") {
+        exclude(group = "org.apache.logging.log4j", module = "log4j-slf4j2-impl")
+    }
 
-	annotationProcessor("io.fabric8:crd-generator-apt:6.13.4")
+    annotationProcessor("io.fabric8:crd-generator-apt:6.13.4")
 
-	testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
 
-	testImplementation("org.awaitility:awaitility:4.2.2")
+    testImplementation("org.awaitility:awaitility:4.2.2")
 
-	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
 
 tasks.withType<Test> {
-	useJUnitPlatform()
+    useJUnitPlatform()
 }
