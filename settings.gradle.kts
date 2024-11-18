887d7d0eafe647a50ca6e11cab0a0fa38bbc3c24
Patrick Schwager <patrick.schwager@telekom.de>
2024-11-18 18:16:36 +0100

    fix header format

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,11 +1,11 @@
-pluginManagement {
-    plugins {
-        kotlin("jvm") version "2.0.21"
-    }
-}
 /*
  * SPDX-FileCopyrightText: 2024 Deutsche Telekom AG
  *
  * SPDX-License-Identifier: Apache-2.0
  */
+pluginManagement {
+    plugins {
+        kotlin("jvm") version "2.0.21"
+    }
+}
 rootProject.name = "lmos-operator"
